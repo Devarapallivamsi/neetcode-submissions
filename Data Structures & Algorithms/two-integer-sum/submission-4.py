4852cd5c204efcbf18361a3c841294e234226c65
Vamsi Devarapalli <54593063+Devarapallivamsi@users.noreply.github.com>
2026-08-10 10:39:55 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsIdx = [[val,idx] for idx,val in enumerate(nums)]
+
+        numsIdxSortednums = sorted(numsIdx)
+        
+        i = 0
+        j = len(numsIdxSortednums) - 1
+        while i < j:
+            # accessing values
+            _sum = numsIdxSortednums[i][0] + numsIdxSortednums[j][0]
+            if _sum == target:
+                return [min(numsIdxSortednums[i][1],numsIdxSortednums[j][1]),
+                        max(numsIdxSortednums[i][1],numsIdxSortednums[j][1])]
+            elif _sum < target:
+                i += 1
+            else:
+                j -= 1
+                
+       
+
+
+        
+        
