118e30b2f342f7f7dea97c9b2552e89602b1390d
Vamsi Devarapalli <54593063+Devarapallivamsi@users.noreply.github.com>
2026-09-19 13:00:42 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        n = len(s)
+        if n % 2 != 0:
+            return False
+        mystack = []
+        match = {'}':'{',']':'[',')':'('}
+        for i in s:
+            if i in {'[','(','{'}:
+                mystack.append(i)
+            else:
+                if len(mystack) == 0:
+                    return False
+                if match[i] != mystack.pop():
+                    return False                
+        return len(mystack) == 0
+        
